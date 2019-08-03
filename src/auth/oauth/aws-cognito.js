3481aba2cdd8f10215a74a97a06c01ae15b78b31
Awood549 <Austin.wood1776@live.com>
2019-08-03 16:21:06 -0700

    routes

--- src/auth/oauth/aws-cognito.js
+++ src/auth/oauth/aws-cognito.js
@@ -1,1 +1,23 @@
 
+const express = require('express');
+const amazinRouter = express.Router();
+
+
+amazinRouter.get('/amzOAuth', (req,res,next) => {
+  let code = req.code;
+  console.log(code);
+  res.status(200).send('SomeShit');
+});
+
+amazinRouter.post('/endpoint', (req,res,next) => {
+  let token = req.token;
+  
+  res.status(200).send('Success');
+});
+
+
+//Write a /get to catch the "Code" coming from amazon
+//send that code to their token server
+// POST to catch the incoming token and send it to their server to get the persons info
+//receive the client info and send it to the front end
+
